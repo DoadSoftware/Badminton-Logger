cb3671306b7f0d62f41a6fecbadc2812bca1e54c
Doad Software Department <98625971+DoadSoftware@users.noreply.github.com>
2022-08-11 20:26:50 +0530

    changes in code

--- src/com/badminton/model/Rules.java
+++ src/com/badminton/model/Rules.java
@@ -22,14 +22,8 @@ public class Rules
   @Column(name = "RuleId")
   private int ruleId;
 
-  @Column(name = "PROMPT")
-  private String prompt;
-
   @Column(name = "TEXT1")
   private String text1;
-  
-  @Column(name = "TEXT2")
-  private String text2;
 
   public Rules() {
 	  super();
@@ -48,28 +42,12 @@ public void setRuleId(int ruleId) {
 	this.ruleId = ruleId;
 }
 
-public String getPrompt() {
-	return prompt;
-}
-
-public void setPrompt(String prompt) {
-	this.prompt = prompt;
-}
-
 public String getText1() {
 	return text1;
 }
 
 public void setText1(String text1) {
 	this.text1 = text1;
-}
-
-public String getText2() {
-	return text2;
-}
-
-public void setText2(String text2) {
-	this.text2 = text2;
 }
 
 }
